80efcde3a39281e56cf5c93a52e0c34eea6a45d1
aashish suwal <asuwal@myseneca.ca>
2024-05-17 17:46:41 -0400

    initiate first commit

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+/*********************************************************************************
+* WEB700 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: aashish suwal Student ID: 161760236 Date: 5/17/2024
+*
+********************************************************************************/
+
+serverVerbs = [GET, GET, GET, POST, GET, POST];
+serverPaths = [/, /about, /contacts, /login, /panel, /logout];
+serverResponse = ['Welcome to WEB700 Assignment 1', 'This assignment was prepared by aashish suwal', 'Student Name: Aashish suwal Student Email: asuwal@myseneca.ca', 'User Logged in', "Main panel", 'Logout Complete' ];
