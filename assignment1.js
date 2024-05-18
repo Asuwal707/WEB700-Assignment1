3a21b37d7aaca9f9691702a13b2908c4fed81544
aashish suwal <asuwal@myseneca.ca>
2024-05-18 17:30:19 -0400

    nice

--- assignment1.js
+++ assignment1.js
@@ -7,7 +7,25 @@
 * Name: aashish suwal Student ID: 161760236 Date: 5/17/2024
 *
 ********************************************************************************/
+// creating gllobal variabls 
+serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
+serverPaths = ["/", "/about", "/contacts", "/login", "/panel", "/logout"];
+serverResponse = ["Welcome to WEB700 Assignment 1", "This assignment was prepared by aashish suwal", "Student Name: Aashish suwal Student Email: asuwal@myseneca.ca", "User Logged in", "Main panel", "Logout Complete"];
 
-serverVerbs = [GET, GET, GET, POST, GET, POST];
-serverPaths = [/, /about, /contacts, /login, /panel, /logout];
-serverResponse = ['Welcome to WEB700 Assignment 1', 'This assignment was prepared by aashish suwal', 'Student Name: Aashish suwal Student Email: asuwal@myseneca.ca', 'User Logged in', "Main panel", 'Logout Complete' ];
+// creating our http request as an arrow function :) (tried smth else lol)
+
+// using const coz we wont be reassigning them. httpverb and path are just parameters for serververbs and serverpaths.
+const httpRequest = (httpverb, path) => {
+    for (let i = 0; i < serverpaths.length; i++) {
+        if(serverVerbs[i] === httpverb && serverPaths[i] === paths ) {
+            return `200: ${serverResponse[i]}`;
+        }
+    }
+
+    return `404: unable to process ${httpverb} request for ${path}`; // ` is a template literal allowing us to embed expression inside ${} string
+}
+
+// testing our function 
+
+
+
